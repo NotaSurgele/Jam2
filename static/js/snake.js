2eb9d6badb32678735a32c6ef8b07666aa76b3c1
Jojolions <jojolions@localhost.localdomain>
2022-03-26 22:33:24 +0400

    naive enum for direction added

--- static/js/snake.js
+++ static/js/snake.js
@@ -16,11 +16,17 @@ let config = {
     }
 };
 
-var game = new Phaser.Game(config);
+const UP = 0;
+const RIGHT = 1;
+const DOWN = 2;
+const LEFT = 3;
+let game = new Phaser.Game(config);
 let player;
+let direction = 1;
 
 function preload()
 {
+    direction = RIGHT;
     this.load.image('sky', 'http://labs.phaser.io/assets/skies/deepblue.png');
     this.load.spritesheet('player', '../static/images/ghost.png',
         {frameWidth: 32, frameHeight: 32});
@@ -32,20 +38,12 @@ function create()
     player = this.physics.add.sprite(100, 32, 'player');
 
     this.anims.create({
-        key: 'left',
+        key: 'move',
         frames: this.anims.generateFrameNumbers('player', {start: 0, end: 3}),
         frameRate: 5,
         repeat: -1
     })
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 3}),
-        frameRate: 5,
-        repeat: -1
-    });
-
-    player.setBounce(0, 0);
     player.setCollideWorldBounds(true);
 }
 
@@ -53,21 +51,36 @@ function update()
 {
     let cursor = this.input.keyboard.createCursorKeys();
 
-    if (cursor.left.isDown) {
+    if (cursor.left.isDown && direction != RIGHT)
+        direction = LEFT;
+    else if (cursor.right.isDown && direction != LEFT)
+        direction = RIGHT;
+    else if (cursor.up.isDown && direction != DOWN)
+        direction = UP;
+    else if (cursor.down.isDown && direction != UP)
+        direction = DOWN;
+    switch (direction) {
+    case LEFT:
         player.setVelocityY(0);
         player.setVelocityX(-100);
-        player.anims.play('left', true);
-    } else if (cursor.right.isDown) {
+        player.anims.play('move', true);
+        break;
+    case RIGHT:
         player.setVelocityY(0);
         player.setVelocityX(100);
-        player.anims.play('right', true);
-    } else if (cursor.up.isDown) {
+        player.anims.play('move', true);
+        break;
+    case UP:
         player.setVelocityX(0);
         player.setVelocityY(-100);
-        player.anims.play('right', true);
-    } else if (cursor.down.isDown) {
+        player.anims.play('move', true);
+        break;
+    case DOWN:
         player.setVelocityX(0);
         player.setVelocityY(100);
-        player.anims.play('right', true);
+        player.anims.play('move', true);
+        break;
+    default:
+        break;
     }
 }
