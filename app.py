ea62e89b8f20f69e5cfe88d7d2457a45d9718652
Jojolions <jojolions@localhost.localdomain>
2022-03-26 16:08:45 +0400

    python server done with flask rout: html file and any

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import *
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return "<h1>Welcome to our website</h1>"
+
+
+@app.route("/html")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/file')
+def render():
+    if 'filename' in request.args:
+        myfilename = request.args.get('filename')
+        return render_template(myfilename)
+    else:
+        return "No input file specified"
+
+
+@app.route("/<name>")
+def escape_route(name):
+    return f"Hello, {escape(name)}!"
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8080)
