1c3e6f04a07866c757a69f30f9a89368d73ae63b
yahaya <sandati.yahaya@epitech.eu>
2022-03-27 23:31:47 +0400

    click

--- static/script/paint.js
+++ static/script/paint.js
@@ -13,12 +13,13 @@ var config = {
 
 var game = new Phaser.Game(config);
 var color = 0x0643C7;
+var size;
+let s = 10;
 
 function preload() {
 }
 
 function create() {
-    let s = 10;
     let swap = 1;
     var circle = new Phaser.Geom.Circle(0, 0, s, color);
     var rect = new Phaser.Geom.Rectangle(100, 100, 800, 600, 0x05008B);
@@ -27,14 +28,19 @@ function create() {
     var cl = new Phaser.Geom.Circle(1100, 150, 20);
     var cir = new Phaser.Geom.Circle(1100, 200, 20);
     var cirv = new Phaser.Geom.Circle(1100, 250, 20);
+    var up = new Phaser.Geom.Triangle(1100, 300, 1070, 330, 1130, 330);
+    var down = new Phaser.Geom.Triangle(1130, 400, 1070, 400, 1100, 430);
     var pal = this.add.rectangle(500, 850, 800, 100, 0xEBFAFA);
     var sgraphic = this.add.graphics({fillStyle: {lineWidth: 9, color: 0x05008B}, lineStyle: {color: 0x05008B}});
     var graphics = this.add.graphics();
+    var graphict = this.add.graphics({fillStyle: {lineWidth: 9, color: 0x05008B}, lineStyle: {color: 0x05008B}});
 
     this.add.text(940, 60, 'TOOLS', { fontSize: '32px', fill: '#0F1154'});
     this.add.text(950, 135, 'Clear', { fontSize: '32px', fill: '#00BCFF'});
     this.add.text(950, 190, 'Full', { fontSize: '32px', fill: '#00BCFF'});
     this.add.text(950, 235, 'Empty', { fontSize: '32px', fill: '#00BCFF'});
+    this.add.text(950, 350, 'Size', { fontSize: '32px', fill: '#00BCFF'});
+    size = this.add.text(1080, 350, s, { fontSize: '32px', fill: '#05008B'});
     this.add.text(100, 750, 'PALETTE', { fontSize: '32px', fill: '#0F1154'});
 
     pal.setStrokeStyle(4, 0x05008B);
@@ -47,23 +53,26 @@ function create() {
     graphics.fillStyle(0x1EF7FF);
     graphics.strokeCircleShape(cl);
     graphics.fillCircleShape(cl);
+    graphict.fillTriangleShape(up);
+    graphict.fillTriangleShape(down);
 
-    this.input.on('pointermove', function (pointer) {
-        if (Phaser.Geom.Circle.Contains(cl, pointer.x, pointer.y)) {
-            if (pointer.isDown)
+    this.input.on('pointerdown', function (pointer) {
+        if (pointer.isDown && Phaser.Geom.Circle.Contains(cl, pointer.x, pointer.y))
                 location.reload();
-        }
-        if (Phaser.Geom.Circle.Contains(cir, pointer.x, pointer.y)) {
-            if (pointer.isDown)
+        if (pointer.isDown && Phaser.Geom.Circle.Contains(cir, pointer.x, pointer.y))
                 swap = 1;
-        }
-        if (Phaser.Geom.Circle.Contains(cirv, pointer.x, pointer.y)) {
-            if (pointer.isDown)
+        if (pointer.isDown && Phaser.Geom.Circle.Contains(cirv, pointer.x, pointer.y))
                 swap = 2;
-        }
+        if (pointer.isDown && Phaser.Geom.Triangle.Contains(up, pointer.x, pointer.y))
+            s = s + 1;
+        if (pointer.isDown && Phaser.Geom.Triangle.Contains(down, pointer.x, pointer.y) && s > 1)
+            s = s - 1;
+    }, this);
+    this.input.on('pointermove', function (pointer) {
         if (pointer.isDown && Phaser.Geom.Rectangle.Contains(rect, pointer.x, pointer.y)) {
             graphics.lineStyle(1, color);
             graphics.fillStyle(color);
+            circle
             if (pointer.x < 100 + (2 * s) || pointer.y < 100 + (2 * s)) {
                 circle.setPosition(pointer.x + s, pointer.y + s);
             } else {
@@ -78,5 +87,5 @@ function create() {
 }
 
 function update() {
-
+    size.setText(s);
 }
