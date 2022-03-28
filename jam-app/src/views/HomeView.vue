67b26922234879d350ed9a20fdcd9c23b161c00f
ghost-byte <pierre.cardia@epitech.eu>
2022-03-28 16:01:02 +0400

    add vue

---
+++ jam-app/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="vh-100 text-center" align-v="center">
+    <b-button pill variant="primary" size="lg">Start snake</b-button>
+  </div>
+  <!-- <div class="home"></div> -->
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  // components: {
+  // },
+};
+</script>
